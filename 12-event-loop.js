e6b283647c5467feeb2b9ee67ee25b96ad210b06
manijhaneswar1 <manijhaneswar@gmail.com>
2025-01-07 22:24:28 +0530

    eventLoop Code

---
+++ 12-event-loop.js
@@ -0,0 +1,14 @@
+const http=require('http');
+
+const server =http.createServer((req,res)=>{
+    console.log('request EVENT');
+    res.end('Hello World!');
+})
+
+server.listen(8080,()=>{
+    console.log('server listening on 8080')
+});
+
+
+// Event Loop that the main thread is never blocked, allowing
+// node.js to handle a large number of concurrent operations efficiently.
